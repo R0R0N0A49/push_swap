bf22f2b22bd2ac2f8e371c3c4b454f2e8d1799e0
R0R0N0A49 <150067904+R0R0N0A49@users.noreply.github.com>
2023-12-19 14:30:39 +0100

    Add files via upload

--- test_algo/test_algo.c
+++ test_algo/test_algo.c
@@ -11,47 +11,50 @@
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	ft_push_swap(char **tab_a)
+void	ft_push_swap(int *tab_a, int len_a)
 {
-	char	**tab_b;
-	int		len_b;
+	int	*tab_b;
+	int	len_b = 0;
 
 	tab_b = NULL;
-	while (ft_verif_order(tab_a, tab_b, 1))
+	while (ft_verif_order(tab_a, len_a, len_b, 1))
 	{
-		while (ft_verif_order(tab_a, tab_b, 0))
+		while (ft_verif_order(tab_a, len_a, len_b, 0))
 		{
-			if (ft_verif_first_a(tab_a) == 1)
-				tab_a = ft_rotate_a(tab_a);
-			else if (ft_verif_first_a(tab_a) == -1)
-				tab_a = ft_reverse_a(tab_a);
-			else if (ft_atoi(tab_a[0]) > ft_atoi(tab_a[1]))
-				tab_a = ft_swap(tab_a, 'a');
-			else
+			if (len_a > 0 && ft_verif_first(tab_a, len_a - 1) == 1)
+				tab_a = ft_rotate_a(tab_a, 'a');
+			else if (len_a > 0 && ft_verif_first(tab_a, len_a - 1) == -1)
+				tab_a = ft_reverse(tab_a, 'a', len_a - 1);
+			else if (len_a > 0 && tab_a[0] > tab_a[1])
+				tab_a = ft_swap(tab_a, 'a', len_a);
+			else if (len_a > 0)
 			{
-				tab_b = ft_push(tab_b, tab_a, 'b');
-				tab_a = ft_malloc_push(tab_a);
+				tab_b = ft_push(tab_b, tab_a, 'b', len_b);
+				tab_a = ft_malloc_push(tab_a, len_a);
+				len_a--;
+				len_b++;
 				break ;
 			}
 		}
-		if (tab_b && ft_verif_revers_order(tab_b))
+		if (tab_b && ft_verif_revers_order(tab_b, len_b))
 		{
-			len_b = ft_len_tab(tab_b);
-			if (len_b > 1 && ft_verif_first_a(tab_b) == -1)
+			if (len_b > 1 && ft_verif_first(tab_b, len_b - 1) == -1)
 				tab_b = ft_rotate_b(tab_b);
-			else if (len_b > 1 && ft_verif_first_a(tab_b) == 1)
-				tab_b = ft_reverse_b(tab_b);
-			else if (len_b > 1 && ft_atoi(tab_b[0]) > ft_atoi(tab_b[1]))
-				tab_b = ft_swap(tab_b, 'b');
+			else if (len_b > 1 && ft_verif_first(tab_b, len_b - 1) == 1)
+				tab_b = ft_reverse(tab_b, 'b', len_b - 1);
+			else if (len_b > 1 && tab_b[0] > tab_b[1])
+				tab_b = ft_swap(tab_b, 'b', len_b);
 		}
-		if (tab_b && ft_verif_order(tab_a, tab_b, 0) == 0)
+		if (tab_b && ft_verif_order(tab_a, len_a, len_b, 0) == 0)
 		{
-			tab_a = ft_push(tab_a, tab_b, 'a');
-			tab_b = ft_malloc_push(tab_b);
+			tab_a = ft_push(tab_a, tab_b, 'a', len_a);
+			tab_b = ft_malloc_push(tab_b, len_b);
+			len_a++;
+			len_b--;
 		}
 	}
+	ft_display(tab_a, tab_b, len_a, len_b);
 	if (tab_b)
-		ft_free(tab_b);
-	ft_free(tab_a);
+		free(tab_b);
+	free(tab_a);
 }
